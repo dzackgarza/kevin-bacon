86e5b4e56e73078ff0a5c0238326c84e0b224fef
Zack <dzackgarza@gmail.com>
2014-06-09 15:17:15 -0700

    Closer to the original theoretical model.

--- src/New/BFSTree.java
+++ src/New/BFSTree.java
@@ -1,20 +1,47 @@
 package New;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
-public class BFSTree<T> {
-    private Node<T> root;
+public class BFSTree<Vertex> {
+    public Node<Vertex> rootNode;
+    public Node<Vertex>[] children;
+    public HashMap<Vertex, Node<Vertex>> treeHash;
 
-    public BFSTree(T rootData) {
-        root = new Node<T>();
-        root.data = rootData;
-        root.children = new ArrayList<Node<T>>();
+    public BFSTree(Vertex rootVertex) {
+        rootNode = new Node<Vertex>(rootVertex, null);
+        rootNode.children = new ArrayList<Node<Vertex>>();
+        treeHash.put(rootVertex, rootNode);
     }
 
-    public static class Node<T> {
-        private T data;
-        private Node<T> parent;
-        private List<Node<T>> children;
+    public static class Node<Vertex> {
+        public Vertex data = null;
+        public Node<Vertex> parent = null;
+        public String movieWithParent = null;
+        public List<Node<Vertex>> children = null;
+        
+        public Node(Vertex thisVertex, Node<Vertex> parentNode) {
+        	data = thisVertex;
+        	parent = parentNode;
+        }
     }
+    
+     public void addNode(Vertex v, Vertex parent, String movie) {
+    	Node<Vertex> n = treeHash.get(v);
+    	Node<Vertex> p = treeHash.get(parent);
+    	if (n == null) {
+    		n = new Node<Vertex>(v, p);
+    		p.children.add(n);
+    		n.parent = p;
+    		n.movieWithParent = movie;
+    	}
+     }
+     
+     public Node<Vertex> getNode(Vertex v) {
+    	 Node<Vertex> r = treeHash.get(v);
+    	 if (r == null)
+    		 r = new Node<Vertex>(v, null);
+    	 return r;
+     }
 }
